d5590949a5faf3e92a1a7905323ab7a8035b2a11
ben <ben.oneill.nz@gmail.com>
2019-04-05 00:43:02 -0400

    s

---
+++ web/myChart.js
@@ -0,0 +1,24 @@
+
+function renderChart(data, labels) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'This week',
+                data: data,
+            }]
+        },
+    });
+}
+
+
+function ff() {
+        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
+        labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+        renderChart(data, labels);
+    }
+ff()
+
+
