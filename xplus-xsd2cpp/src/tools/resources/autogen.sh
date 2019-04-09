34397de707557f729cad8c08e0c21dde7f9c156f
Ben O'Neill <boneill@rnet-tech.com>
2019-04-09 16:31:11 -0400

    Reformat

--- xplus-xsd2cpp/src/tools/resources/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-mkdir -p m4
-
-test -n "$srcdir" || srcdir=`dirname "$0"`
-test -n "$srcdir" || srcdir=.
-
-autoreconf --force --install --verbose "$srcdir"
-test -n "$NOCONFIGURE" || "$srcdir/configure"  --prefix=`pwd`/build "$@"
