e34eb22f946074277d409d1971761f0441bd5e27
Ben <boneill@rnet-tech.com>
2020-02-18 02:39:10 -0500

    Remove the C++ includes from VnV.h. If the user wants to implement an engine, they should directly include the interface class, etc

--- injection/include/VnV.h
+++ injection/include/VnV.h
@@ -3,6 +3,10 @@
 #ifndef VV_VNV_H
 #define VV_VNV_H
 
+// VnV.h represents the include required to use the library at runtime. This includes all
+// the functions required to initialize and register the library. It should be included and
+// compilable using a C compiler to insure it can be used in C and C++.
+
 // C Interface for the runtime functions.
 #include "c-interfaces/runtime-interface.h"
 
@@ -15,19 +19,5 @@
 
 // C Interface for the Logging components of VnV
 #include "c-interfaces/logging-interface.h"
-
-// TODO: Decide on including these interfaces in the VnV.h header include for all C++ projects. This would be fine,
-// but, the interfaces include a header only json library. We could forward declare the json class to remove that dependency,
-// but, then the user would be forced to include the "json.hpp" when implementing any plugin. At that point, its probably more
-// intuitive to foce the user to include the interface/i*.h header instead.
-#if __cplusplus
-
- # include "interfaces/ioutputengine.h"
- # include "interfaces/itransform.h"
- # include "interfaces/iserializer.h"
- # include "interfaces/itest.h"
- # include "interfaces/iunittester.h"
-
-#endif //CPLUS_PLUS
 
 #endif // GAURD
