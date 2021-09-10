d66868456a5e36ded8550e8e4b9bda41e9e5e0cb
ben <boneill@rnet-tech.com>
2021-09-10 16:32:11 -0400

    data

--- injection/include/version/version.h
+++ injection/include/version/version.h
@@ -6,12 +6,12 @@
   const char* __vnv__verison_info_##PNAME() {                              \
     return "{"                                                             \
        "\"git_retrieved_state\" : true," \
-       "\"git_head_sha1\"       : \"b43b81436e7c48a146c62e0f3a50d88eea2e9813\"," \
+       "\"git_head_sha1\"       : \"7ee23e6b7cdd492d595a75dc8ca5ff7cfec2eaa5\"," \
        "\"git_dirty\"           : \"true," \
        "\"git_author\"          : \"ben\"," \
        "\"git_author_email\"    : \"boneill@rnet-tech.com\"," \
-       "\"git_commit_date\"     : \"2021-09-09 15:47:03 -0400\"," \
-       "\"git_describe\"        : \"b43b814\"," \
+       "\"git_commit_date\"     : \"2021-09-10 14:51:46 -0400\"," \
+       "\"git_describe\"        : \"7ee23e6\"," \
        "\"c_compiler\"          : \"/usr/bin/cc\"," \
        "\"c_compiler_version\"  : \"10.3.0\"," \
        "\"cxx_compiler\"        : \"/usr/bin/c++\"," \
