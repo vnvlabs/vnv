2d1a14dcf6d0a3a3e17d59c5f607632802f16a66
ben <boneill@rnet-tech.com>
2021-10-22 14:52:28 -0400

    Info

--- injection/include/version/version.h
+++ injection/include/version/version.h
@@ -6,12 +6,12 @@
   const char* __vnv__verison_info_##PNAME() {                              \
     return "{"                                                             \
        "\"git_retrieved_state\" : true," \
-       "\"git_head_sha1\"       : \"b677d8089003ef00418f54a22b025b2ef52fe497\"," \
-       "\"git_dirty\"           : \"true," \
+       "\"git_head_sha1\"       : \"98bafbd2b6a4faf496ef0eae4440043b9bbbc54c\"," \
+       "\"git_dirty\"           : \"false," \
        "\"git_author\"          : \"ben\"," \
        "\"git_author_email\"    : \"boneill@rnet-tech.com\"," \
-       "\"git_commit_date\"     : \"2021-10-22 13:07:16 -0400\"," \
-       "\"git_describe\"        : \"b677d80\"," \
+       "\"git_commit_date\"     : \"2021-10-22 14:09:47 -0400\"," \
+       "\"git_describe\"        : \"98bafbd\"," \
        "\"c_compiler\"          : \"/bin/x86_64-linux-gnu-gcc-10\"," \
        "\"c_compiler_version\"  : \"10.3.0\"," \
        "\"cxx_compiler\"        : \"/bin/x86_64-linux-gnu-g++-10\"," \
