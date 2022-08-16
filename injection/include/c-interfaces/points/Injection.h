0cb917c6bc7d202edbeb8dc86ebf641d99520862
jburz2001 <jburz002@ucr.edu>
2022-08-16 13:44:56 -0400

    some documentation of macros (variadic args, etc)

--- injection/include/c-interfaces/points/Injection.h
+++ injection/include/c-interfaces/points/Injection.h
@@ -11,15 +11,21 @@
 #  define EVERYONE(...) FOR_EACH(DOIT, __VA_ARGS__)
 
 // Injection Points
+// "..." refers to "variadic arguments" throughout this file
+//   variadic args enable the user to input an arbitrary number of arguments 
+//   to the respective "variadic function" that has "..." as a parameter
 
-#  define INJECTION_POINT_C(PNAME, COMM, NAME, callback, ...)            \
+#  define INJECTION_POINT_C(PNAME, COMM, NAME, callback, ...) \
     _VnV_injectionPoint(COMM, VNV_STR(PNAME), VNV_STR(NAME), VNV_FUNCTION_SIG, __FILE__, __LINE__, callback, \
                         EVERYONE(__VA_ARGS__) VNV_END_PARAMETERS_S);
 
+//special case of INJECTION_POINT_C where ...
+//  callback=NULL
+//  ...=__VA_ARGS__, where "__VA_ARGS__" represents the variadic arguments that the user inputs as an argument for the "..." parameter
 #  define INJECTION_POINT(PNAME, COMM, NAME, ...) \
     INJECTION_POINT_C(PNAME, COMM, NAME, NULL, __VA_ARGS__)
 
-#  define INJECTION_LOOP_BEGIN_C(PNAME, COMM, NAME, callback, ...)             \
+#  define INJECTION_LOOP_BEGIN_C(PNAME, COMM, NAME, callback, ...) \
     _VnV_injectionPoint_begin(COMM, VNV_STR(PNAME), VNV_STR(NAME), VNV_FUNCTION_SIG, __FILE__, __LINE__, callback, \
                               EVERYONE(__VA_ARGS__) VNV_END_PARAMETERS_S);
 
@@ -32,6 +38,7 @@
 #  define INJECTION_LOOP_ITER(PNAME, NAME, STAGE) \
     _VnV_injectionPoint_loop(VNV_STR(PNAME), VNV_STR(NAME), VNV_STR(STAGE), __FILE__, __LINE__);
 
+//FIXME remove ) after STAGE ?
 #  define INJECTION_LOOP_ITER_D(PNAME, NAME, STAGE) \
     _VnV_injectionPoint_loop(VNV_STR(PNAME), VNV_STR(NAME), STAGE), __FILE__, __LINE__);
 
@@ -50,8 +57,18 @@
 #  define Register_Injection_Point(PNAME, NAME, PARAMETERS) \
     _VnV_registerInjectionPoint(PNAME, NAME, PARAMETERS);
 
-VNVEXTERNC void _VnV_injectionPoint(VnV_Comm comm, const char* packageName,
-                                    const char* id, struct VnV_Function_Sig pretty, const char* fname, int line,
+// VNVEXTERNC is a VnV macro that substitutes   extern "C"  for VNVEXTERNC if the filetype is .cpp
+//   if the filetype is not .cpp, then it substitutes whitespace for VNVEXTERNC
+// When you state that a function has extern "C" ilnkage in C++, 
+//   the C++ compiler does not add argument/parameter type information 
+//   to the name used for linkage
+// https://stackoverflow.com/questions/1041866/what-is-the-effect-of-extern-c-in-c
+VNVEXTERNC void _VnV_injectionPoint(VnV_Comm comm, 
+                                    const char* packageName,
+                                    const char* id, 
+                                    struct VnV_Function_Sig pretty, 
+                                    const char* fname, 
+                                    int line,
                                     injectionDataCallback* callback, ...);
 
 VNVEXTERNC void _VnV_injectionPoint_begin(VnV_Comm comm,
@@ -71,6 +88,7 @@ VNVEXTERNC void _VnV_registerInjectionPoint(const char* packageName,
                                             const char* id,
                                             const char* parameters_str);
 
+// if not using VnV, then replace VnV macros with whitespace
 #else
 
 #  define INJECTION_POINT(...)
